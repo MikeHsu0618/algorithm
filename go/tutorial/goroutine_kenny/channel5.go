3f105c86eac836bd3679f6bd7aae4cedf7b8db6a
mike <a3698521tw11@gmail.com>
2022-05-27 13:14:29 +0800

    feat: before repo pattern

--- go/tutorial/goroutine_kenny/channel5.go
+++ go/tutorial/goroutine_kenny/channel5.go
@@ -10,13 +10,13 @@ func main() {
 	intChan := make(chan int, num)
 
 	// 單線程 -> 等到值全都進去後才會往下進行
-	// for i := 0; i < num; i++ {
-	// 	intChan <- i
-	// }
-	//
-	// for i := 0; i < num; i++ {
-	// 	fmt.Println(<-intChan)
-	// }
+	for i := 0; i < num; i++ {
+		intChan <- i
+	}
+
+	for i := 0; i < num; i++ {
+		fmt.Println(<-intChan)
+	}
 
 	// 多協程的基本應用
 	wg := new(sync.WaitGroup)
