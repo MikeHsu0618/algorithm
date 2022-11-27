8ba560a9208c7c60b619ae3c2875a2342e6ce74b
mike <a3698521tw11@gmail.com>
2022-11-27 15:59:21 +0800

    leetcode: add 104 maximum-depth-of-binary-tree

---
+++ go/leetcode/editor/en/104 maximum-depth-of-binary-tree.go
@@ -0,0 +1,68 @@
+package main
+
+import "container/list"
+
+//Given the root of a binary tree, return its maximum depth.
+//
+// A binary tree's maximum depth is the number of nodes along the longest path
+//from the root node down to the farthest leaf node.
+//
+//
+// Example 1:
+//
+//
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 3
+//
+//
+// Example 2:
+//
+//
+//Input: root = [1,null,2]
+//Output: 2
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in the tree is in the range [0, 10⁴].
+// -100 <= Node.val <= 100
+//
+//
+// Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 90
+//66 👎 149
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	depth := 0
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		depth++
+	}
+	return depth
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
