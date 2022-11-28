88ee473c52c6f4de16f96f6b42ba26189168eb44
mike <a3698521tw11@gmail.com>
2022-11-28 23:37:00 +0800

    leetcode: add 101 symmetric-tree

---
+++ go/leetcode/editor/en/101 symmetric-tree.go
@@ -0,0 +1,62 @@
+package main
+
+//Given the root of a binary tree, check whether it is a mirror of itself (i.e.,
+// symmetric around its center).
+//
+//
+// Example 1:
+//
+//
+//Input: root = [1,2,2,3,4,4,3]
+//Output: true
+//
+//
+// Example 2:
+//
+//
+//Input: root = [1,2,2,null,3,null,3]
+//Output: false
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in the tree is in the range [1, 1000].
+// -100 <= Node.val <= 100
+//
+//
+//
+//Follow up: Could you solve it both recursively and iteratively?
+//
+// Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 11
+//650 👎 261
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 解法一： 使用相反走法來, 兩兩比較左樹右樹節點是否相同
+func isSymmetric(root *TreeNode) bool {
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root.Left, root.Right)
+	for len(queue) > 0 {
+		left, right := queue[0], queue[1]
+		queue = queue[2:]
+		if left == nil && right == nil {
+			continue
+		}
+		if left == nil || right == nil || left.Val != right.Val {
+			return false
+		}
+		queue = append(queue, left.Left, right.Right, left.Right, right.Left)
+	}
+	return true
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
