c5a8c37d235cd64c18dfce0d8488a93b55bfd226
mike <a3698521tw11@gmail.com>
2022-12-01 14:02:17 +0800

    leetcode: add 100 same-tree

---
+++ go/leetcode/editor/en/100 same-tree.go
@@ -0,0 +1,69 @@
+package main
+
+//Given the roots of two binary trees p and q, write a function to check if
+//they are the same or not.
+//
+// Two binary trees are considered the same if they are structurally identical,
+//and the nodes have the same value.
+//
+//
+// Example 1:
+//
+//
+//Input: p = [1,2,3], q = [1,2,3]
+//Output: true
+//
+//
+// Example 2:
+//
+//
+//Input: p = [1,2], q = [1,null,2]
+//Output: false
+//
+//
+// Example 3:
+//
+//
+//Input: p = [1,2,1], q = [1,1,2]
+//Output: false
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in both trees is in the range [0, 100].
+// -10⁴ <= Node.val <= 10⁴
+//
+//
+// Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 76
+//82 👎 162
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, p, q)
+	for len(queue) > 0 {
+		left, right := queue[0], queue[1]
+		queue = queue[2:]
+		if left == nil && right == nil {
+			continue
+		}
+		if left == nil || right == nil || left.Val != right.Val {
+			return false
+		}
+		// 同時比對兩顆樹
+		queue = append(queue, left.Left, right.Left, left.Right, right.Right)
+	}
+	return true
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
