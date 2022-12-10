2bf2a3538d7ae30501e559844028fdf16d979dbe
mike <a3698521tw11@gmail.com>
2022-12-10 17:47:54 +0800

    leetcode: add 1757 recyclable-and-low-fat-products

---
+++ go/leetcode/editor/en/1757 recyclable-and-low-fat-products.go
@@ -0,0 +1,57 @@
+package main
+//Table: Products 
+//
+// 
+//+-------------+---------+
+//| Column Name | Type    |
+//+-------------+---------+
+//| product_id  | int     |
+//| low_fats    | enum    |
+//| recyclable  | enum    |
+//+-------------+---------+
+//product_id is the primary key for this table.
+//low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low 
+//fat and 'N' means it is not.
+//recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is 
+//recyclable and 'N' means it is not. 
+//
+// 
+//
+// Write an SQL query to find the ids of products that are both low fat and 
+//recyclable. 
+//
+// Return the result table in any order. 
+//
+// The query result format is in the following example. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: 
+//Products table:
+//+-------------+----------+------------+
+//| product_id  | low_fats | recyclable |
+//+-------------+----------+------------+
+//| 0           | Y        | N          |
+//| 1           | Y        | Y          |
+//| 2           | N        | Y          |
+//| 3           | Y        | Y          |
+//| 4           | N        | N          |
+//+-------------+----------+------------+
+//Output: 
+//+-------------+
+//| product_id  |
+//+-------------+
+//| 1           |
+//| 3           |
+//+-------------+
+//Explanation: Only products 1 and 3 are both low fat and recyclable.
+// 
+//
+// Related Topics Database 👍 691 👎 69
+
+
+//There is no code of Go type for this problem
+
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
