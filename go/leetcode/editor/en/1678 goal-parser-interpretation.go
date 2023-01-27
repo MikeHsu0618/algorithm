4ddd11312688b1c8bd45a75d49641b66572094bc
mike <a3698521tw11@gmail.com>
2023-01-27 16:29:47 +0800

    leetcode: add 1678 goal-parser-interpretation

---
+++ go/leetcode/editor/en/1678 goal-parser-interpretation.go
@@ -0,0 +1,71 @@
+package main
+
+//You own a Goal Parser that can interpret a string command. The command
+//consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will
+//interpret "G" as the string "G", "()" as the string "o", and "(al)" as the
+//string "al". The interpreted strings are then concatenated in the original order.
+//
+// Given the string command, return the Goal Parser's interpretation of command.
+//
+//
+//
+// Example 1:
+//
+//
+//Input: command = "G()(al)"
+//Output: "Goal"
+//Explanation: The Goal Parser interprets the command as follows:
+//G -> G
+//() -> o
+//(al) -> al
+//The final concatenated result is "Goal".
+//
+//
+// Example 2:
+//
+//
+//Input: command = "G()()()()(al)"
+//Output: "Gooooal"
+//
+//
+// Example 3:
+//
+//
+//Input: command = "(al)G(al)()()G"
+//Output: "alGalooG"
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= command.length <= 100
+// command consists of "G", "()", and/or "(al)" in some order.
+//
+//
+// Related Topics String 👍 1141 👎 79
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func interpret(command string) string {
+	res := ""
+
+	for i := 0; i < len(command); i++ {
+		if command[i] == 'G' {
+			res += "G"
+			continue
+		}
+
+		if command[i] == '(' && command[i+1] == ')' {
+			res += "o"
+			i++
+			continue
+		}
+
+		res += "al"
+		i += 3
+	}
+
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
