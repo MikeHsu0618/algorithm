6dd0d18bc525a79a5ad3854fb39821ce070f555f
mike <a3698521tw11@gmail.com>
2023-01-31 20:37:10 +0800

    leetcode: add 1290 convert-binary-number-in-a-linked-list-to-integer

---
+++ go/leetcode/editor/en/1290 convert-binary-number-in-a-linked-list-to-integer.go
@@ -0,0 +1,55 @@
+package main
+
+//Given head which is a reference node to a singly-linked list. The value of
+//each node in the linked list is either 0 or 1. The linked list holds the binary
+//representation of a number.
+//
+// Return the decimal value of the number in the linked list.
+//
+// The most significant bit is at the head of the linked list.
+//
+//
+// Example 1:
+//
+//
+//Input: head = [1,0,1]
+//Output: 5
+//Explanation: (101) in base 2 = (5) in base 10
+//
+//
+// Example 2:
+//
+//
+//Input: head = [0]
+//Output: 0
+//
+//
+//
+// Constraints:
+//
+//
+// The Linked List is not empty.
+// Number of nodes will not exceed 30.
+// Each node's value is either 0 or 1.
+//
+//
+// Related Topics Linked List Math 👍 3403 👎 145
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *ListNode) (decimal int) {
+	for head != nil {
+		decimal = (decimal * 2) + head.Val
+		head = head.Next
+	}
+
+	return
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
